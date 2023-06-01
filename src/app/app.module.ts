8d1e7c39b9deb202c70d3ff0ddb87990832fb6f8
Jhetal0001 <jhetal00@gmail.com>
2023-05-31 23:47:19 -0500

    Se crea proyecto angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FaIconLibrary, FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    BrowserAnimationsModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    library.addIconPacks(fas, far, fab);
+  }
+}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,36 @@
+import { Component, ViewChild, ViewContainerRef } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+
+  isSyncAnimated = true;
+  magicLevel!:any;
+  notification: number = 0;
+
+  input(event: any) {
+    this.magicLevel = (event.target as HTMLInputElement).value
+  }
+
+  onClick(){
+    this.notification++
+  }
+
+  @ViewChild('host', { static: true, read: ViewContainerRef })
+  container!: ViewContainerRef;
+
+  createIcon() {
+    const componentRef = this.container.createComponent(FaIconComponent);
+    componentRef.instance.icon = faUser;
+    // Note that FaIconComponent.render() should be called to update the
+    // rendered SVG after setting/updating component inputs.
+    componentRef.instance.render();
+  }
+
+}
